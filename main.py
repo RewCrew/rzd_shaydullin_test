76aef468da5de5817583b70023861877039038ba
RewCrew <kerim94@bk.ru>
2022-07-21 11:54:07 +0300

    added THREADs, splitted logic(all file manipulation now in to_json func), removed unnecessary ELSE and fn.close()

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import json
 import pandas
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired
+from threading import Thread
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -15,22 +17,25 @@ class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
-def to_json(file, file_name):
+def to_json(path, file):
+    time.sleep(2)
     data = {}
-    file_name = file_name.split('.')[0]
+    file_name = file.filename.split('.')[0]
+    file.save(os.path.join(os.getcwd() + '/storage/', secure_filename(file.filename)))
 
-    sheet_names = pandas.ExcelFile(file).sheet_names
+    sheet_names = pandas.ExcelFile(path).sheet_names
     for sheet in sheet_names:
-            excel_data = pandas.read_excel(file, sheet_name=sheet)
+            excel_data = pandas.read_excel(path, sheet_name=sheet)
             json_str = excel_data.to_json(orient = 'records', date_format = 'iso')
             data[sheet] = json.loads(json_str)
-    if os.path.exists(os.getcwd()+'\\json_files\\'+file_name+'.json'):
-        return 'File already exists'
-    else:
-        with open(os.getcwd()+'\\json_files\\'+file_name+'.json', 'w', encoding='utf-8') as jn:
-            jn.write(json.dumps(data, ensure_ascii=False, indent=4))
-            jn.close()
-            return '  file uploaded to server'
+    # if os.path.exists(os.getcwd()+'\\json_files\\'+file_name+'.json'):
+    #     return 'File already exists'
+    # else:
+    with open(os.getcwd()+'\\json_files\\'+file_name+'.json', 'w', encoding='utf-8') as jn:
+        jn.write(json.dumps(data, ensure_ascii=False, indent=4))
+        # jn.close()
+    os.remove(path)
+        # можно убрать клоуз
 
 @app.route('/upload/excel/', methods=['GET',"POST"])
 
@@ -39,10 +44,13 @@ def parser():
     if form.validate_on_submit():
         file = form.file.data
         path = os.path.join(os.getcwd()+'/storage/' + secure_filename(file.filename))
-        file.save(os.path.join(os.getcwd()+'/storage/',secure_filename(file.filename)))
-        result = to_json(path, file.filename)
-        os.remove(path)
-        return "File has been uploaded." + ' '+result
+        if os.path.exists(os.getcwd() + '\\json_files\\' + file.filename.split('.')[0] + '.json'):
+            return jsonify('File already exist')
+        # file.save(os.path.join(os.getcwd()+'/storage/',secure_filename(file.filename)))
+        Thread(target=to_json(path, file)).start()
+        # to_json(path, file.filename)
+        # os.remove(path)
+        return jsonify("File has been uploaded.")
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
